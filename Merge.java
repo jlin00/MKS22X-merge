3d78082b5131cdec50849200d440f33ffd57e4d0
Jackie Lin <jlin00@stuy.edu>
2019-03-15 10:03:25 -0400

    added some code to helper function

--- Merge.java
+++ Merge.java
@@ -1,11 +1,14 @@
 public class Merge{
   /*sort the array from least to greatest value. This is a wrapper function*/
   public static void mergesort(int[] data){
-
+    mergesortH(data, 0, data.length);
   }
 
-  //helper function for merge sort 
-  public static void mergesortH(int[] data, int lo, int hi){
+  //helper function for merge sort
+  public static void mergesortH(int[] data, int start, int end){
+    if (start >= end){
+      return;
+    }
 
   }
 
