3b423d1b5cd9c1371c247e1a1e21a02b303d3188
Jackie Lin <jlin00@stuy.edu>
2019-03-15 09:53:39 -0400

    added method placeholders

---
+++ Merge.java
@@ -0,0 +1,15 @@
+public class Merge{
+  /*sort the array from least to greatest value. This is a wrapper function*/
+  public static void mergesort(int[] data){
+
+  }
+
+  //helper function for merge sort 
+  public static void mergesortH(int[] data, int lo, int hi){
+
+  }
+
+  public static void main(String[] args) {
+
+  }
+}
